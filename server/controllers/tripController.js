e4e0d475c2c53d7bcb13e7d3e00bfd8de583e203
Sharon <Sharonpatterson808@gmail.com>
2024-09-25 15:48:58 -0400

    started controllers for saving trip

---
+++ server/controllers/tripController.js
@@ -0,0 +1,12 @@
+const trip = requires('server/models/tripModes.js')
+
+const tripController = {
+    saveTrip: function(req, res, next){
+        // logic here
+    },
+    getTrip: function(req, res, next){
+        // logic here
+    }
+}
+
+module.exports = tripController
---
+++ server/models/tripModes.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const tripSchema = mongoose.Schema({
+  //logic here  
+  //settings and way point
+})
+
+const Trip = mongoose.model('Trip', tripSchema)
+module.exports = Trip
