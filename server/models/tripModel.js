d0f0277c5af1b725ea4ac5ff7276e1f04774f2de
Sharon <Sharonpatterson808@gmail.com>
2024-09-25 14:33:37 -0400

    fixed env bug

---
+++ server/models/tripModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+//schema
+const tripSchema = mangoose.Schema({
+    
+})
+
+const savedTrips = mangoose.model('savedTrips', tripSchema)
+module.exports = savedTrips
