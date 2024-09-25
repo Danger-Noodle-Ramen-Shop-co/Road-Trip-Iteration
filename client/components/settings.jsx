9717df962ffb51cdf5cf1bc91e4fabd0945ea510
David <davidjs314@yahoo.com>
2024-09-25 17:10:36 -0400

    turns fetch for settings and waypointContainer into a RTK Querry using Redux-Toolkit

--- client/components/settings.jsx
+++ client/components/settings.jsx
@@ -1,19 +1,23 @@
-import React, { useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   addWaypoint,
   updateDestination,
   updateOrigin,
   updateStep,
-  updateWaypoints,
 } from '../features/genSettings/genSettingsSlice';
 
 const Settings = () => {
   //States for tracking journey from origin to destination
-  const origin = useSelector((state) => state.genSettings.origin);
-  const destination = useSelector((state) => state.genSettings.destination);
-  const step = useSelector((state) => state.genSettings.step);
-  const waypointStr = useSelector((state) => state.genSettings.waypointStr);
+  // const origin = useSelector((state) => state.genSettings.origin);
+  // const destination = useSelector((state) => state.genSettings.destination);
+  // const step = useSelector((state) => state.genSettings.step);
+  // const waypointStr = useSelector((state) => state.genSettings.waypointStr);
+
+  const [origin, setOrigin] = useState('');//set's up local state
+  const [destination, setDestination] = useState('');
+  const [step, setStep] = useState(0);
+
   const dispatch = useDispatch();
 
   //create a reference
@@ -32,74 +36,23 @@ const Settings = () => {
     inputRef.current.value = '';
   }
 
-  function chunkRoute() {
-    async function getData() {
-      //Will need to add API key, figure out how to get
-      //Fetching from the API the best directions (routes) between origin and destination and waypoints
-      console.log('Making request to server for API');
-      const response = await fetch(
-        `/corsproxy/directions?url=https://maps.googleapis.com/maps/api/directions/json&key=${
-          process.env.GOOGLE_API_KEY
-        }&destination=${destination.replace(' ', '+')}&origin=${origin.replace(
-          ' ',
-          '+'
-        )}${waypointStr}`,
-        {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            Accept: 'application/json',
-          },
-        }
-      );
-      const data = await response.json();
-      // console.dir(data);
-      //    initialize waypoints array = []
-      const waypoints = [];
-      let totalDist = 0;
-      //mile convert to meter
-      const stepInMeters = step * 1609;
-      //    iterate through legs array, set totalDist = 0
-      //        iterate through each leg's steps array,
-      console.log('num legs = ', data.routes[0].legs.length);
-      console.log('num routes = ', data.routes.length);
-      if (data.routes.length === 0) return;
-      //only accumulate distance when passing the leg of the travel
-      for (const leg of data.routes[0].legs) {
-        for (const step of leg.steps) {
-          // for each step, add its distance to the totalDist
-          totalDist += step.distance.value;
-          // if totalDist is greater than chunkLength:
-          if (totalDist > stepInMeters) {
-            // add the startLocation (place id maybe?) from the step to our own waypoints array
-            waypoints.push(
-              `${step.start_location.lat},${step.start_location.lng}`
-            );
-
-            // reset the totalDist to 0
-            totalDist = 0;
-          }
-        }
-      }
-      console.dir(waypoints);
-      dispatch(updateWaypoints(waypoints));
-      //    include waypoints array into the embedded map src url
-    }
-    // check for step if its nothing, return early
-    if (!step) return;
-    // fetch request to routes api using origin and destination
-    getData();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(updateOrigin(origin));
+    dispatch(updateDestination(destination));
+    dispatch(updateStep(step));
   }
 
   return (
     <div style={styles.settings}>
-      <form style={styles.top}>
+      <form style={styles.top} onSubmit={handleSubmit}>
         <label htmlFor='from'>From:</label>
         <input
           id='from'
           type='text'
           value={origin}
-          onChange={(e) => dispatch(updateOrigin(e.target.value))}
+          onChange={(e) => setOrigin(e.target.value)}
         ></input>
 
         <label htmlFor='to'>To:</label>
@@ -107,23 +60,20 @@ const Settings = () => {
           id='to'
           type='text'
           value={destination}
-          onChange={(e) => dispatch(updateDestination(e.target.value))}
+          onChange={(e) => setDestination(e.target.value)}
         ></input>
-      </form>
-
-      <form style={styles.bottom}>
         <label htmlFor='steps'>Chunk Trip By Miles:</label>
         <input
           id='steps'
           type='number'
           value={step}
-          onChange={(e) => dispatch(updateStep(e.target.value))}
+          onChange={(e) => setStep(e.target.value)}
         ></input>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            chunkRoute();
-          }}
+        <button type='submit'
+          // onClick={(e) => {//maybe switch to on submit so the state doesn't change everytime someone types so rtk querry works
+          //   e.preventDefault();
+          //   chunkRoute();
+          // }}
         >
           Find Stops
         </button>
